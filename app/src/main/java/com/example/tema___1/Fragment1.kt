28eec32e13dc495f7ec44533fb71d4c0a6bb3329
DorinUscoiu1 <dorin.uscoiu@student.unitbv.ro>
2024-04-30 15:42:40 +0300

    Update

--- app/src/main/java/com/example/tema___1/Fragment1.kt
+++ app/src/main/java/com/example/tema___1/Fragment1.kt
@@ -22,52 +22,53 @@ class Fragment1 : Fragment()
     }
     private fun getSampleAnimalList(): List<Animal>
     {
-        val animals = mutableListOf<Animal>()
-        animals.add(Animal("Leu", "Africa"))
-        animals.add(Animal("Elefant", "Africa"))
-        animals.add(Animal("Koala", "Australia"))
-        animals.add(Animal("Panda", "Asia"))
-        animals.add(Animal("Vultur", "America de Nord"))
-        animals.add(Animal("Condor", "America de Sud"))
-        animals.add(Animal("Pinguin", "Antarctica"))
-        animals.add(Animal("Vulpe", "Europa"))
-        animals.add(Animal("Tigru", "Asia"))
-        animals.add(Animal("Cangur", "Australia"))
-        animals.add(Animal("Urs Grizzly", "America de Nord"))
-        animals.add(Animal("Armadillo", "America de Sud"))
-        animals.add(Animal("Orca", "Antarctica"))
-        animals.add(Animal("Veverita", "Europa"))
-        animals.add(Animal("Girafa", "Africa"))
-        animals.add(Animal("Kangaroo", "Australia"))
-        animals.add(Animal("Bizon", "America de Nord"))
-        animals.add(Animal("Anaconda", "America de Sud"))
-        animals.add(Animal("Foca", "Antarctica"))
-        animals.add(Animal("Caine", "Europa"))
-        animals.add(Animal("Bufnita", "Africa"))
-        animals.add(Animal("Kookaburra", "Australia"))
-        animals.add(Animal("Elan", "America de Nord"))
-        animals.add(Animal("Puma", "America de Sud"))
-        animals.add(Animal("Lup", "Europa"))
-        animals.add(Animal("Hipopotam", "Africa"))
-        animals.add(Animal("Wombat", "Australia"))
-        animals.add(Animal("Urs Panda", "Asia"))
-        animals.add(Animal("Capibara", "America de Sud"))
-        animals.add(Animal("Albatros", "Antarctica"))
-        animals.add(Animal("Zebra", "Africa"))
-        animals.add(Animal("Wallaby", "Australia"))
-        animals.add(Animal("Coyote", "America de Nord"))
-        animals.add(Animal("Jaguar", "America de Sud"))
-        animals.add(Animal("Mistret", "Europa"))
-        animals.add(Animal("Leopard", "Africa"))
-        animals.add(Animal("Bursuc", "America de Nord"))
-        animals.add(Animal("Foca Weddell", "Antarctica"))
-        animals.add(Animal("Liliac", "America de Sud"))
-        animals.add(Animal("Elefant Indian", "Asia"))
-        animals.add(Animal("Gorila", "Africa"))
-        animals.add(Animal("Camila", "Australia"))
-        animals.add(Animal("Veverita Rosie", "Europa"))
-        animals.add(Animal("Cangur Mare", "Australia"))
-        animals.add(Animal("Veverita Zburatoare", "Europa"))
+        val animals = mutableListOf<Animal>(
+            Animal("Leopard", "Africa"),
+            Animal("Tucan", "America de Sud"),
+            Animal("Lemur", "Africa"),
+            Animal("Panda Rosu", "Asia"),
+            Animal("Vultur", "Europa"),
+            Animal("Koala", "Australia"),
+            Animal("Iepure Polar", "Antarctica"),
+            Animal("Coiot", "America de Nord"),
+            Animal("Elefant", "Africa"),
+            Animal("Condor", "America de Sud"),
+            Animal("Urangutan", "Asia"),
+            Animal("Pinguin", "Antarctica"),
+            Animal("Vulpe Polara", "Europa"),
+            Animal("Cangur", "Australia"),
+            Animal("Tatu", "America de Sud"),
+            Animal("Leu", "Africa"),
+            Animal("Capibara", "America de Sud"),
+            Animal("Tigru Siberian", "Asia"),
+            Animal("Foca", "Antarctica"),
+            Animal("Caine Lup", "Europa"),
+            Animal("Cangur Alb", "Australia"),
+            Animal("Bufnita", "Europa"),
+            Animal("Coiot Mexican", "America de Nord"),
+            Animal("Gorila de Munte", "Africa"),
+            Animal("Wallaby(Cangur pitic)", "Australia"),
+            Animal("Puma", "America de Sud"),
+            Animal("Elefant Indian", "Asia"),
+            Animal("Zebra", "Africa"),
+            Animal("Albatros", "Antarctica"),
+            Animal("Veverita", "Europa"),
+            Animal("Dingo", "Australia"),
+            Animal("Daubentonia madagascariensis", "Africa"),
+            Animal("Condor din California", "America de Nord"),
+            Animal("Rinocer Indian", "Asia"),
+            Animal("Hiena", "Africa"),
+            Animal("Cangur Tasmanian", "Australia"),
+            Animal("Ocelot", "America de Sud"),
+            Animal("Panda Gigant", "Asia"),
+            Animal("Foca Ross", "Antarctica"),
+            Animal("Caine Ciobanesc", "Europa"),
+            Animal("Liliac", "America de Sud"),
+            Animal("Gorila", "Africa"),
+            Animal("Veverita1", "Europa"),
+            Animal("Cangur Rosu", "Australia"),
+            Animal("Burunduc", "Europa")
+        )
         return animals
     }
 }
